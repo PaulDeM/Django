80548e56c34e1de55afd9812d9bc0210702dc933
Paul <pawel.mordasiewicz@wp.pl>
2018-01-29 21:00:48 +0100

    general layout

--- posts/urls.py
+++ posts/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.posts_list),
-    url(r'^(?P<slug>[\w-]+)/$', views.post_detail),
+    url(r'^$', views.posts_list, name='list'),
+    url(r'^(?P<slug>[\w-]+)/$', views.post_detail, name='detail'),
 ]
