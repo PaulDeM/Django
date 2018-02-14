e6c690cae1a8297ff1a21c83d6e0d87962d31cb3
Paul <pawel.mordasiewicz@wp.pl>
2018-02-14 15:07:38 +0100

    link to posts

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from django.http import HttpResponse
-
+from .models import Post
 
 def posts_list(request):
     posts = Post.objects.all().order_by('date')
@@ -9,4 +9,7 @@ def posts_list(request):
 
 
 def post_detail(request, slug):
-    return HttpResponse(request, slug)
+    #return HttpResponse(request, slug)
+    post = Post.objects.get(slug=slug)
+    return render(request, 'posts/posts_detail.html', {'post': post})
+
