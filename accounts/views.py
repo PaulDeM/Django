87b4657f2c53aaa38310540bf63beecf0f72601f
Paul <pawel.mordasiewicz@wp.pl>
2018-03-12 20:09:48 +0100

    login users function

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
 def signup_view(request):
@@ -7,6 +8,8 @@ def signup_view(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.save()
+            login(request, user)
             return redirect('posts:list')
             # sending to the posts list
     else:
@@ -17,6 +20,8 @@ def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
+            user = form.get_user()
+            login(request, user)
             return redirect('posts:list')
     else:
         form = AuthenticationForm()
